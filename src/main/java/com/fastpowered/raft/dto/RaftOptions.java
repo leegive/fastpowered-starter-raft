5ab965ea57a354d9e67e535e5ef731c7937c8920
lishangjin <li.shangjin@icloud.com>
2019-02-15 14:16:46 +0800

    :construction: Raft Node impl...

--- src/main/java/com/fastpowered/raft/dto/RaftOptions.java
+++ src/main/java/com/fastpowered/raft/dto/RaftOptions.java
@@ -1,4 +1,12 @@
 package com.fastpowered.raft.dto;
 
+import lombok.Data;
+
+@Data
 public class RaftOptions {
+
+    private long electionTime;
+
+    private long heartBeatTick;
+
 }
--- src/main/java/com/fastpowered/raft/protocol/impl/AbstractNode.java
+++ src/main/java/com/fastpowered/raft/protocol/impl/AbstractNode.java
@@ -6,10 +6,29 @@ import com.fastpowered.raft.protocol.Node;
 
 public abstract class AbstractNode implements Node, LifeCycle {
 
-    protected RaftOptions options;
+    /**
+     * 选举时间间隔基数
+     */
+    private volatile long electionTime = 15 * 1000;
+
+    /**
+     * 心跳时间间隔基数
+     */
+    private long heartBeatTick = 5 * 1000;
+
+    /**
+     * 上次选举时间
+     */
+    private volatile long preElectionTime = 0;
+
+    /**
+     * 上次心跳时间
+     */
+    private volatile long preHeartBeatTime = 0;
 
     @Override
     public void setOptions(RaftOptions options) {
-        this.options = options;
+        this.electionTime = options.getElectionTime();
+        this.heartBeatTick = options.getHeartBeatTick();
     }
 }
